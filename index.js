33c72581c188321d9a3803d50a1521dcbea307da
Abhishek Kumar <72213318+AK-ABHISHEK-KUMAR@users.noreply.github.com>
2022-12-15 17:05:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const todoRoute = require("./routes/todosRoute");
 
-const LOCAL_DB_URL = "mongodb://0.0.0.0:27017/TodoList";
+const DB_URL = process.env.DB_URL || "mongodb://0.0.0.0:27017/TodoList";
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "*"}));
+app.use(cors({origin: "https://task-planner.onrender.com"}));
 
 // app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/todos", todoRoute);
 
 mongoose
-  .connect(process.env.DB_URL || LOCAL_DB_URL, {
+  .connect(DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
